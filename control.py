a275af4071ee5f40e78951f5111228c5d39e7392
Tyrone Novillo <tyminobra@outlook.es>
2025-07-28 11:11:58 -0500

    Update control.py

--- control.py
+++ control.py
@@ -1,116 +1,67 @@
-# Copyright 2011-2012 James McCauley
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at:
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Copyright del autor original
+# Licencia Apache 2.0
 
 """
-An L2 learning switch.
-
-It is derived from one written live for an SDN crash course.
-It is somwhat similar to NOX's pyswitch in that it installs
-exact-match rules for each flow.
+Este módulo implementa un switch de nivel 2 con aprendizaje (L2 learning switch).
+Se basa en un ejemplo de curso SDN. Es similar al módulo 'pyswitch' de NOX,
+ya que instala reglas exactas para cada flujo detectado.
 """
 
 from pox.core import core
 import pox.openflow.libopenflow_01 as of
-from pox.lib.util import dpid_to_str, str_to_dpid
-from pox.lib.util import str_to_bool
+from pox.lib.util import dpid_to_str, str_to_dpid, str_to_bool
 from pox.lib.packet.tcp import tcp
 from pox.lib.packet.ipv4 import ipv4
 import time
 
+# Obtener el logger para imprimir mensajes en consola
 log = core.getLogger()
 
-# We don't want to flood immediately when a switch connects.
-# Can be overriden on commandline.
+# Retardo inicial para evitar inundaciones al conectar el switch
 _flood_delay = 0
 
 class LearningSwitch (object):
   """
-  The learning switch "brain" associated with a single OpenFlow switch.
-
-  When we see a packet, we'd like to output it on a port which will
-  eventually lead to the destination.  To accomplish this, we build a
-  table that maps addresses to ports.
-
-  We populate the table by observing traffic.  When we see a packet
-  from some source coming from some port, we know that source is out
-  that port.
-
-  When we want to forward traffic, we look up the desintation in our
-  table.  If we don't know the port, we simply send the message out
-  all ports except the one it came in on.  (In the presence of loops,
-  this is bad!).
-
-  In short, our algorithm looks like this:
-
-  For each packet from the switch:
-  1) Use source address and switch port to update address/port table
-  2) Is transparent = False and either Ethertype is LLDP or the packet's
-     destination address is a Bridge Filtered address?
-     Yes:
-        2a) Drop packet -- don't forward link-local traffic (LLDP, 802.1x)
-            DONE
-  3) Is destination multicast?
-     Yes:
-        3a) Flood the packet
-            DONE
-  4) Port for destination address in our address/port table?
-     No:
-        4a) Flood the packet
-            DONE
-  5) Is output port the same as input port?
-     Yes:
-        5a) Drop packet and similar ones for a while
-  6) Install flow table entry in the switch so that this
-     flow goes out the appopriate port
-     6a) Send the packet out appropriate port
+  Clase que implementa la lógica de un switch con aprendizaje.
+  Permite asociar direcciones MAC con puertos y tomar decisiones de reenvío.
   """
-  def __init__ (self, connection, transparent):
-    # Switch we'll be adding L2 learning switch capabilities to
-    self.connection = connection
-    self.transparent = transparent
 
-    # Our table
+  def __init__ (self, connection, transparent):
+    # Conexión con el switch OpenFlow
+    self.connection = connection
+    # Si el switch es transparente (procesa o no tráfico LLDP)
+    self.transparent = transparent
+    # Tabla de aprendizaje MAC -> puerto
     self.macToPort = {}
 
-    # We want to hear PacketIn messages, so we listen
-    # to the connection
+    # Escuchar eventos provenientes de la conexión con el switch
     connection.addListeners(self)
 
-    # We just use this to know when to log a helpful message
+    # Indica si se ha cumplido el tiempo de espera para inundación
     self.hold_down_expired = _flood_delay == 0
-
-    #log.debug("Initializing LearningSwitch, transparent=%s",
-    #          str(self.transparent))
 
   def _handle_PacketIn (self, event):
     """
-    Handle packet in messages from the switch to implement above algorithm.
+    Método principal que maneja paquetes entrantes (PacketIn) del switch.
+    Se implementa el algoritmo de reenvío según la tabla de direcciones aprendidas.
     """
 
-    packet = event.parsed
-    
+    packet = event.parsed  # Parsear el paquete recibido
+
+    # Extraer información de los protocolos TCP e IPv4 si existen
     tcp_pkt = packet.find('tcp')
     ip_pkt = packet.find('ipv4')
 
-    # Detectar tráfico MQTT (Puerto TCP 1883)
+    # ==== INGENIERÍA DE TRÁFICO: PRIORIZAR MQTT (TCP 1883) ====
     if tcp_pkt is not None and ip_pkt is not None:
         if tcp_pkt.dstport == 1883 or tcp_pkt.srcport == 1883:
+            # Si es tráfico MQTT, buscar el puerto de destino conocido
             dst_port = self.macToPort.get(packet.dst)
             if dst_port is not None and dst_port != event.port:
                 log.info("Tráfico MQTT detectado: %s:%s -> %s:%s por puerto %s" % (
                     ip_pkt.srcip, tcp_pkt.srcport, ip_pkt.dstip, tcp_pkt.dstport, dst_port))
 
+                # Crear una regla de flujo con alta prioridad para el tráfico MQTT
                 msg = of.ofp_flow_mod()
                 msg.priority = 50000  # Prioridad alta
                 msg.match = of.ofp_match.from_packet(packet, event.port)
@@ -119,44 +70,43 @@ class LearningSwitch (object):
                 msg.actions.append(of.ofp_action_output(port=dst_port))
                 msg.data = event.ofp
                 self.connection.send(msg)
-                return  # ¡Ya se manejó!
-
+                return  # Terminar el procesamiento aquí
             else:
+                # Si no se conoce el puerto de destino aún, se inunda el paquete
                 flood("Tráfico MQTT sin puerto conocido — flooding")
                 return
 
+    # ==== FUNCIONES INTERNAS ====
+
     def flood (message = None):
-      """ Floods the packet """
+      """
+      Función para inundar el paquete por todos los puertos excepto el de entrada.
+      Utilizada cuando no se conoce el puerto destino.
+      """
       msg = of.ofp_packet_out()
       if time.time() - self.connection.connect_time >= _flood_delay:
-        # Only flood if we've been connected for a little while...
-
         if self.hold_down_expired is False:
-          # Oh yes it is!
           self.hold_down_expired = True
-          log.info("%s: Flood hold-down expired -- flooding",
+          log.info("%s: Fin del retardo de inundación — se permite flooding",
               dpid_to_str(event.dpid))
 
-        if message is not None: log.debug(message)
-        #log.debug("%i: flood %s -> %s", event.dpid,packet.src,packet.dst)
-        # OFPP_FLOOD is optional; on some switches you may need to change
-        # this to OFPP_ALL.
+        if message is not None:
+          log.debug(message)
+
+        # Acción: enviar a todos los puertos (excepto el entrante)
         msg.actions.append(of.ofp_action_output(port = of.OFPP_FLOOD))
-      else:
-        pass
-        #log.info("Holding down flood for %s", dpid_to_str(event.dpid))
       msg.data = event.ofp
       msg.in_port = event.port
       self.connection.send(msg)
 
     def drop (duration = None):
       """
-      Drops this packet and optionally installs a flow to continue
-      dropping similar ones for a while
+      Función para descartar paquetes. También puede instalar una regla temporal
+      para descartar flujos similares por un tiempo.
       """
       if duration is not None:
         if not isinstance(duration, tuple):
-          duration = (duration,duration)
+          duration = (duration, duration)
         msg = of.ofp_flow_mod()
         msg.match = of.ofp_match.from_packet(packet)
         msg.idle_timeout = duration[0]
@@ -169,74 +119,85 @@ class LearningSwitch (object):
         msg.in_port = event.port
         self.connection.send(msg)
 
-    self.macToPort[packet.src] = event.port # 1
+    # ==== ALGORITMO DE SWITCH L2 ====
 
-    if not self.transparent: # 2
+    # 1) Aprender la dirección MAC de origen y el puerto por el que llegó
+    self.macToPort[packet.src] = event.port
+
+    # 2) Filtrar tráfico LLDP y de direcciones bridge (si no es transparente)
+    if not self.transparent:
       if packet.type == packet.LLDP_TYPE or packet.dst.isBridgeFiltered():
-        drop() # 2a
+        drop()  # 2a) Se descarta el paquete
         return
 
+    # 3) Si el destino es una dirección multicast, se inunda
     if packet.dst.is_multicast:
-      flood() # 3a
+      flood()  # 3a
     else:
-      if packet.dst not in self.macToPort: # 4
-        flood("Port for %s unknown -- flooding" % (packet.dst,)) # 4a
+      # 4) Si no se conoce el puerto del destino, se inunda
+      if packet.dst not in self.macToPort:
+        flood("Puerto de destino %s desconocido — flooding" % (packet.dst,))
       else:
         port = self.macToPort[packet.dst]
-        if port == event.port: # 5
-          # 5a
-          log.warning("Same port for packet from %s -> %s on %s.%s.  Drop."
-              % (packet.src, packet.dst, dpid_to_str(event.dpid), port))
-          drop(10)
+        # 5) Si el puerto destino es el mismo por donde llegó el paquete, se descarta
+        if port == event.port:
+          log.warning("Mismo puerto de entrada y salida: %s -> %s en %s.%s. Se descarta." %
+              (packet.src, packet.dst, dpid_to_str(event.dpid), port))
+          drop(10)  # Se descartan flujos similares por 10 segundos
           return
-        # 6
-        log.debug("installing flow for %s.%i -> %s.%i" %
+
+        # 6) Se instala una regla de flujo para que este tráfico se reenvíe correctamente
+        log.debug("Instalando flujo %s.%i -> %s.%i" %
                   (packet.src, event.port, packet.dst, port))
         msg = of.ofp_flow_mod()
         msg.match = of.ofp_match.from_packet(packet, event.port)
         msg.idle_timeout = 10
         msg.hard_timeout = 30
         msg.actions.append(of.ofp_action_output(port = port))
-        msg.data = event.ofp # 6a
+        msg.data = event.ofp  # 6a) Se reenvía este paquete también
         self.connection.send(msg)
 
 
 class l2_learning (object):
   """
-  Waits for OpenFlow switches to connect and makes them learning switches.
+  Clase que escucha conexiones de switches y les asigna la lógica de LearningSwitch.
   """
+
   def __init__ (self, transparent, ignore = None):
     """
-    Initialize
-
-    See LearningSwitch for meaning of 'transparent'
-    'ignore' is an optional list/set of DPIDs to ignore
+    Constructor. Inicializa el modo transparente y lista de switches ignorados.
     """
     core.openflow.addListeners(self)
     self.transparent = transparent
     self.ignore = set(ignore) if ignore else ()
 
   def _handle_ConnectionUp (self, event):
+    """
+    Evento que se ejecuta cuando un switch se conecta al controlador.
+    Si no está en la lista de ignorados, se le asigna un LearningSwitch.
+    """
     if event.dpid in self.ignore:
-      log.debug("Ignoring connection %s" % (event.connection,))
+      log.debug("Ignorando conexión %s" % (event.connection,))
       return
-    log.debug("Connection %s" % (event.connection,))
+    log.debug("Conexión recibida: %s" % (event.connection,))
     LearningSwitch(event.connection, self.transparent)
 
 
 def launch (transparent=False, hold_down=_flood_delay, ignore = None):
   """
-  Starts an L2 learning switch.
+  Función que lanza el controlador desde línea de comandos POX.
+  Permite configurar si el switch es transparente, el retardo de inundación y switches ignorados.
   """
   try:
     global _flood_delay
     _flood_delay = int(str(hold_down), 10)
     assert _flood_delay >= 0
   except:
-    raise RuntimeError("Expected hold-down to be a number")
+    raise RuntimeError("El parámetro hold-down debe ser un número")
 
   if ignore:
     ignore = ignore.replace(',', ' ').split()
     ignore = set(str_to_dpid(dpid) for dpid in ignore)
 
+  # Registrar la aplicación l2_learning en el núcleo de POX
   core.registerNew(l2_learning, str_to_bool(transparent), ignore)
